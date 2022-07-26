d1e5af290b0b3a31a8056cb9d48daf54fcd868b4
Manav Panchal <manav03panchal@gmail.com>
2022-07-27 00:06:28 +0300

    no new changes

--- TheGrandAdventure.py
+++ TheGrandAdventure.py
@@ -1,15 +1,21 @@
-class Stack:
-    def __init__(self):
-        self.stack=[]
-    def push(self, someItem):
-        self.stack.append(someItem)
-    def pop(self):
-        self.stack.pop(-1)
-    def isEmpty(self):
-        if len(self.stack)==0:
-            return True
-    def peek(self):
-        return self.stack[-1]
-
-for i in range(input()):
-    pass
+f = open('1.txt','r')
+flines = f.readlines()
+flines=[i.strip() for i in flines]
+# class Stack:
+#     def __init__(self):
+#         self.stack=[]
+#     def push(self, someItem):
+#         self.stack.append(someItem)
+#     def pop(self):
+#         self.stack.pop(-1)
+#     def isEmpty(self):
+#         if len(self.stack)==0:
+#             return True
+#     def peek(self):
+#         return self.stack[-1]
+numIters = int(flines[0])
+flines.remove(flines[0])
+newData=[]
+for i in range(len(flines)):
+    newData.append(flines[i].replace('.',''))
+print(newData)
